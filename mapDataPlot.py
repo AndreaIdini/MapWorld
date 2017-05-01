19bab25a82264d2b7738e1714e9cd6de84ad98e7
Andrea Idini <andrea.idini@gmail.com>
2017-05-01 09:54:29 +0100

    Fixed proportions

--- mapDataPlot.py
+++ mapDataPlot.py
@@ -1,5 +1,10 @@
+#######################################################################
 ## Function to plot choropleth maps of data on top of shapefiles map ##
-## A. Idini 2017 ##
+##                         A. Idini 2017                             ##
+#######################################################################
+import numpy as np
+import matplotlib
+from mpl_toolkits.basemap import Basemap
 
 #- function mP_data takes in input filename and datafile outputs patches for plotting -#
 #- builds up a whole Basemap--based Panda dataframe with the list of patches,
@@ -12,14 +17,13 @@
 #-         ['area_m']  area in sq meters
 #-         ['area_km'] area in sq km
 #- ...
-def mP_data(flnm, df, imp = None):
+def mP_data(flnm, colName, df, imp = None):
     num_colors = 10
 
     if imp is None:
         import pandas as pd
         import matplotlib.pyplot as plt
         import matplotlib.colors as colors
-        from mpl_toolkits.basemap import Basemap
         from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
         from pysal.esda.mapclassify import Natural_Breaks as nb
         from matplotlib.collections import PatchCollection
@@ -59,22 +63,13 @@ def mP_data(flnm, df, imp = None):
         urcrnrlat=coords[3] + extra + 0.01 * h,
         lat_ts=0,
         resolution='i',
-        suppress_ticks=True)
+        suppress_ticks=False)
 
     m.readshapefile(
         flnm,
         'map',
         color='none',
         zorder=2)
-    #
-    # m.drawmapscale(
-    #     coords[0] + 0.08, coords[1] + 0.015,
-    #     coords[0], coords[1],
-    #     10.,
-    #     barstyle='fancy', labelstyle='simple',
-    #     fillcolor1='w', fillcolor2='#555555',
-    #     fontcolor='#555555',
-    #     zorder=5)
 
 # Setup a dataframe that imports the dictionary of map properties and then
 # selects rows corresponding to the imported dataframe df
@@ -82,6 +77,7 @@ def mP_data(flnm, df, imp = None):
 
     for dicti in m.map_info:
         temp_df = temp_df.append(pd.Series(dicti),ignore_index=True)
+#    print temp_df; quit()
 
     i1 = temp_df.set_index('label').index
     i2 = df.set_index('Sector').index
@@ -111,14 +107,14 @@ def mP_data(flnm, df, imp = None):
     print '... built map frame ...'
 ## Calculates Jenks natural breaks, over notnull column
 
-    prices = df_map[df_map['2016 Q3'].notnull()]['2016 Q3'].tolist()
+    prices = df_map[df_map[colName].notnull()][colName].tolist()
 
     breaks = nb ( prices,
         initial=250, #number of initial solutions in iteriative Jenks algo
         k=num_colors )
 
     print 'Calculting Jenks Natural breaks for binning'
-    jenbin = pd.DataFrame({'jenks_bins': breaks.yb}, index=df_map[df_map['2016 Q3'].notnull()].index)
+    jenbin = pd.DataFrame({'jenks_bins': breaks.yb}, index=df_map[df_map[colName].notnull()].index)
     df_map = df_map.join(jenbin)
     df_map.jenks_bins.fillna(-1, inplace=True)
 
@@ -129,7 +125,7 @@ def mP_data(flnm, df, imp = None):
     df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(
         x,
         fc='0.33',
-        edgecolor='w', lw=.2,
+        edgecolor='black', lw=.33,
         alpha=.9))
 
     print 'Last touches of color, with Jenks'
@@ -140,10 +136,114 @@ def mP_data(flnm, df, imp = None):
 
     norm = colors.Normalize()
     pc = PatchCollection(df_map['patches'], match_original=True)
-    print df_map['jenks_bins'].values
-    print norm(df_map['jenks_bins'].values)
-    print colorm(norm(df_map['jenks_bins'].values))
 
     pc.set_facecolor(colorm(norm(df_map['jenks_bins'].values)))
 
-    return pc;
+    print 'labels and scale'
+
+    # Prepare the plt plot and axes
+    plt.clf()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, fc='w', frame_on=False)
+
+    # Add a colour bar
+    cb = colorbar_index(num_colors, colorm, shrink=0.5)
+    cb.ax.tick_params(labelsize=6)
+
+    print "get_bounds"
+
+    newcoords = get_bounds(m,df_map)
+
+    # DOES'T WORK, DOES NOT ACCEPT OSGB COORDS.
+    # m.drawmapscale(
+    #     newcoords[0], newcoords[1],
+    #     coords[0], coords[1],
+    #     10.,
+    #     barstyle='fancy', labelstyle='simple',
+    #     fillcolor1='w', fillcolor2='#555555',
+    #     fontcolor='#555555',
+    #     zorder=5)
+
+    ax.add_collection(pc)
+
+    print "axes and plotting!"
+
+    ax.axis('auto')#; ax.axis('off')
+    #set aspect ratio to latitude-longitude read
+    ax.set_aspect( (newcoords[1]-newcoords[0]) / (newcoords[3]-newcoords[2]) )
+    plt.show()
+
+    return;
+
+def get_bounds(m,df_map):
+    # m = Basemap()
+    # # Read in shapefile, without drawing anything
+    # m.readshapefile("./Map/england_pcs_2012_wgs84", "patches", drawbounds=False)
+
+    # initialize boundaries
+    lon_min = 999999.
+    lon_max = -999999.
+    lat_min = 999999.
+    lat_max = -999999.
+
+    numindex = df_map['SHAPENUM'].tolist()
+    numindex = map(int,numindex)
+
+    for (shape, patch_name) in zip(m.map, m.map_info):
+        if patch_name['SHAPENUM'] in numindex:
+            lon, lat = zip(*shape)
+            if min(lon) < lon_min:
+                lon_min = min(lon)
+            if max(lon) > lon_max:
+                lon_max = max(lon)
+            if min(lat) < lat_min:
+                lat_min = min(lat)
+            if max(lat) > lat_max:
+                lat_max = max(lat)
+
+    return lon_min, lat_min, lon_max, lat_max
+
+
+def colorbar_index(ncolors, cmap, labels=None, **kwargs):
+# Convenience functions for working with colour ramps and bars
+# from Stephan Huegel - 2015 - http://sensitivecities.com/
+
+    """
+    This is a convenience function to stop you making off-by-one errors
+    Takes a standard colour ramp, and discretizes it,
+    then draws a colour bar with correctly aligned labels
+    """
+    cmap = cmap_discretize(cmap, ncolors)
+    mappable = matplotlib.cm.ScalarMappable(cmap=cmap)
+    mappable.set_array([])
+    mappable.set_clim(-0.5, ncolors+0.5)
+    colorbar = matplotlib.pyplot.colorbar(mappable, **kwargs)
+    colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
+    colorbar.set_ticklabels(range(ncolors))
+    if labels:
+        colorbar.set_ticklabels(labels)
+    return colorbar
+
+def cmap_discretize(cmap, N):
+
+    """
+    Return a discrete colormap from the continuous colormap cmap.
+
+        cmap: colormap instance, eg. cm.jet.
+        N: number of colors.
+
+    Example
+        x = resize(arange(100), (5,100))
+        djet = cmap_discretize(cm.jet, 5)
+        imshow(x, cmap=djet)
+
+    """
+    if type(cmap) == str:
+        cmap = matplotlib.pyplot.get_cmap(cmap)
+    colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
+    colors_rgba = cmap(colors_i)
+    indices = np.linspace(0, 1., N + 1)
+    cdict = {}
+    for ki, key in enumerate(('red', 'green', 'blue')):
+        cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
+    return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
--- plot_data.py
+++ plot_data.py
@@ -9,14 +9,15 @@ import numpy as np
 import pandas as pd
 import openpyxl as op
 
-import mapDataPlot
+import mapDataPlot 
 import scrape
 #   -- plot --
 
-mapName="./Map/england_pcs_2012_wgs84"
+mapName   = "./Map/england_pcs_2012_wgs84"
 #dataFile="Pers_Mortage_PCS_2016-q3.xlsx"
-dataFlnm="test.xlsx"
-placeName="London"
+dataFlnm  = "test.xlsx"
+placeName = "London"
+colName   = '2016 Q3'
 
 print 'Search ranges in ' + dataFlnm + ' ...'
 rangesList = scrape.search_Ranges_WorkBk(dataFlnm,placeName)
@@ -26,15 +27,5 @@ print 'Building Dataframe from ' + dataFlnm + ' ...'
 dataFile = scrape.from_Rng_to_DataFrame(dataFlnm,rangesList)
 
 print 'Building map frame using ' + mapName + ' ...'
-df_map = mapDataPlot.mP_data(mapName,dataFile);
 
-fig     = plt.figure()
-ax      = fig.add_subplot(111)
-
-ax.add_collection(df_map)
-
-print "axes and plotting!"
-
-ax.axis('auto')#; ax.axis('off')
-
-plt.show()
+mapDataPlot.mP_data(mapName,colName,dataFile);
